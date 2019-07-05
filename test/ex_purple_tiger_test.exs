487cd29ae6fcd4461cc6d4e40ea3f71af113759d
codeadict <dairon.medina@gmail.com>
2019-07-05 09:27:57 -0400

    Add tests

--- test/ex_purple_tiger_test.exs
+++ test/ex_purple_tiger_test.exs
@@ -1,8 +1,36 @@
 defmodule ExPurpleTigerTest do
   use ExUnit.Case
+  alias ExPurpleTiger.Data
   doctest ExPurpleTiger
 
-  test "greets the world" do
-    assert ExPurpleTiger.hello() == :world
+  @known_text "my ugly input string"
+
+  test "should turn string input into an animal hash" do
+    expected = {:ok, "rapid grey rattlesnake"}
+    assert ExPurpleTiger.animal_hash(@known_text) == expected
+  end
+
+  test "should use a specified separator" do
+    expected = {:ok, "rapid-grey-rattlesnake"}
+    assert ExPurpleTiger.animal_hash(@known_text, separator: "-") == expected
+  end
+
+  test "should support titlecase style" do
+    expected = {:ok, "Rapid Grey Rattlesnake"}
+    assert ExPurpleTiger.animal_hash(@known_text, style: :titlecase) == expected
+  end
+
+  test "should support uppercase style" do
+    expected = {:ok, "RAPID GREY RATTLESNAKE"}
+    assert ExPurpleTiger.animal_hash(@known_text, style: :uppercase) == expected
+  end
+
+  test "should support both separator and style" do
+    expected = {:ok, "rapid_grey_rattlesnake"}
+    assert ExPurpleTiger.animal_hash(@known_text, separator: "_", style: :lowercase) == expected
+  end
+
+  test "should generate different hash for other input" do
+    assert ExPurpleTiger.animal_hash("other") != ExPurpleTiger.animal_hash(@known_text)
   end
 end
